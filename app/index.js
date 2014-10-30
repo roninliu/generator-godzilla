c2fec4c843f0eb9bc0e1f5da89e861fb532e2651
ronin <roninliu@foxmail.com>
2014-10-30 17:52:15 +0800

    支持多模版选择
    加入多模版的选择

--- app/index.js
+++ app/index.js
@@ -4,6 +4,9 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 
+
+
+
 var GodzillaGenerator = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
@@ -26,13 +29,33 @@ var GodzillaGenerator = yeoman.generators.Base.extend({
         type:"input",
         name:"author",
         message:"Please enter your name?",
-        default:"Roninliu"
+        default:"Godzilla"
       },
       {
         type:"input",
         name:"version",
         message:"Please enter your project version?",
         default:"0.0.1"
+      },
+      {
+        type:"rawlist",
+        name:"category",
+        message:"Please select the type you want to create a template?",
+        choices:["QZone act for PC","QZone act for Mobile","Nomarl"],
+        filter:function(val){
+          var type = 0;
+          var select = val.toLowerCase().replace(/\s+/g,"");
+          switch(select){
+            case "qzoneactforpc":
+              return 1;
+            break;
+            case "qzoneactformobile":
+              return 2;
+            break;
+            default:
+              return 3;
+          }
+        }
       }
     ];
 
@@ -40,6 +63,7 @@ var GodzillaGenerator = yeoman.generators.Base.extend({
       this.projectName = props.projectName;
       this.author = props.author;
       this.version = props.version;
+      this.category = props.category;
       done();
     }.bind(this));
   },
@@ -53,11 +77,23 @@ var GodzillaGenerator = yeoman.generators.Base.extend({
       this.dest.mkdir('src/img');
       this.dest.mkdir("src/js");
       this.dest.mkdir("src/slice");
-
-      this.template('src/_index.html', 'src/index.html');
-      this.template('src/css/_style.css', 'src/css/<%= projectName%>.css');
-      this.template('src/js/_common.js', 'src/js/<%= projectName%>.js');
-      
+      switch(this.category){
+        case 1:
+          this.template('src/_index_1.html', 'src/index.html');
+          this.template('src/css/_style.css', 'src/css/<%= projectName%>.css');
+          this.template('src/js/_common.js', 'src/js/<%= projectName%>.js');
+        break;
+        case 2:
+          this.template('src/_index_2.html', 'src/index.html');
+          this.template('src/css/_style.css', 'src/css/<%= projectName%>.css');
+          this.template('src/js/_common.js', 'src/js/<%= projectName%>.js');
+        break
+        default:
+          this.template('src/_index_3.html', 'src/index.html');
+          this.template('src/css/_style_3.css', 'src/css/<%= projectName%>.css');
+          this.template('src/css/_reset.import.css', 'src/css/reset.import.css');
+          this.template('src/js/_common.js', 'src/js/<%= projectName%>.js');
+      }
       this.template('_README.md', 'README.md');
       this.template('_gulpfile.js', 'gulpfile.js');
       this.template('_package.json', 'package.json');
